6ac2b45148b9141f2db7ad5aac4c9b77c0933931
Greg Langford <greg@langford.me>
2016-05-15 15:00:31 +0100

    refactor code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_filter :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -20,12 +22,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article
@@ -36,11 +35,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
@@ -52,4 +49,7 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :body)
     end
 
+    def set_article
+      @article = Article.find(params[:id])
+    end
 end
